40b2523f326fe085b41fbec72822961022d8de89
root <root@edu.novalocal>
2026-01-18 17:13:02 +0000

    2

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,34 +9,37 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
-
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.compute_image_family
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v3"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
+
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 20
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    serial-port-enable = var.vm_web_serial_port_enable
+    ssh-keys           = "${var.vm_web_ssh_user}:${var.vms_ssh_root_key}"
   }
-
 }
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -28,6 +28,55 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "compute_image_family" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "vm_web_name" {
+  type    = string
+  default = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  type    = string
+  default = "standard-v3"
+}
+
+variable "vm_web_cores" {
+  type    = number
+  default = 2
+}
+
+variable "vm_web_memory" {
+  type    = number
+  default = 1
+}
+
+variable "vm_web_core_fraction" {
+  type    = number
+  default = 20
+}
+
+variable "vm_web_preemptible" {
+  type    = bool
+  default = true
+}
+
+variable "vm_web_nat" {
+  type    = bool
+  default = true
+}
+
+variable "vm_web_serial_port_enable" {
+  type    = number
+  default = 1
+}
+
+variable "vm_web_ssh_user" {
+  type    = string
+  default = "ubuntu"
+}
 
 ###ssh vars
 
